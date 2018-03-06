0c62571b6a6b2bd56af09b9c6439f3694857648a
Shaw Vrana <shaw@vranix.com>
2018-03-06 11:37:31 -0800

    Fix up packaging issues
    Was including requirements files in the package so setup.py install
    was failing.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 from pipsqueak import __version__
 
 try:
@@ -10,18 +10,25 @@ except Exception:
     long_description = f.read()
     f.close()
 
-with open('dev-requirements.txt') as f:
-    tests_require = f.read().split('\n')
+def get_requirements(env):
+    with open('requirements-{}.txt'.format(env)) as fp:
+        return [x.strip() for x in fp.read().split('\n') if not x.startswith('#')]
+
+install_requires = get_requirements('base')
+tests_require = get_requirements('test')
+dev_requires = get_requirements('dev')
 
 setup(name='pipsqueak',
       version=__version__,
-      description='Parse pip requirements files',
+      description='Parse pip requirements files and retrieve package information for installed python packages',
       long_description=long_description,
       url='http://github.com/svrana/pipsqueak',
       author='Shaw Vrana',
       author_email='shaw@vranix.com',
       license='MIT',
-      packages=['pipsqueak', 'pipsqueak.pip', 'pipsqueak.pip.vcs'],
+      package_dir={'': 'pipsqueak'},
+      packages=find_packages('pipsqueak'),
+      include_package_data=True,
       classifiers=[
         'License :: OSI Approved :: MIT License',
         'Development Status :: 3 - Alpha',
@@ -29,20 +36,15 @@ setup(name='pipsqueak',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries :: Python Modules',
       ],
-      install_requires=[
-          'six',
-          'packaging',
-          'ipaddress',
-      ],
+      install_requires=install_requires,
       zip_safe=False,
       entry_points={
           'console_scripts': [
               'pipsqueak = pipsqueak.main:main',
           ],
       },
-      tests_require=tests_require,
       extras_require={
-            'testing': tests_require,
+          'tests': tests_require,
+          'dev': dev_requires,
        },
 )
-
